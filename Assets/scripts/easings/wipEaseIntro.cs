ad61f56dc7407ee7f292ef592df2ed1b243c803c
asdas <asd>
2017-03-01 17:10:18 +0100

    Start Working on Easings

---
+++ Assets/scripts/easings/wipEaseIntro.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wipEaseIntro : MonoBehaviour
+{
+
+
+    float initValue;
+    float finalValue;
+    float currentValue;
+
+    int framesCounter;
+    int framesDuration;
+    int initEase;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        initValue = transform.position.y;
+        finalValue = -5.5f;
+        currentValue = initValue;
+
+        framesCounter = 0;
+        framesDuration = 90;
+
+        initEase = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        framesCounter++;
+        if (framesCounter <= framesDuration)
+        {
+            currentValue = Easings.ElasticOut(framesCounter, initValue, finalValue, framesDuration);
+
+            transform.position = new Vector3(0, currentValue, 0);
+        }
+    }
+}
